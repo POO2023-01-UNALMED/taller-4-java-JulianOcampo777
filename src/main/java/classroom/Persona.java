f71dcf242614d48f4bf186cc87a9eba516af3db6
JulianOcampo777 <124713530+JulianOcampo777@users.noreply.github.com>
2023-03-06 15:47:00 -0500

    p3

--- src/main/java/classroom/Persona.java
+++ src/main/java/classroom/Persona.java
@@ -2,13 +2,13 @@ package classroom;
 
 public class Persona {
 
-    final long cedula;
+    /*final*/ long cedula;
     String nombre;
     static int totalPersonas;
     
     static {
         totalPersonas = 0;
-        cedula = 3;
+        //cedula = 3;
     }
 
     public Persona(long cedula, String nombre) {
@@ -18,20 +18,23 @@ public class Persona {
     }
     
     public Persona(String nombre, long cedula) {
-        this.cedula = cedula;
-        this.nombre = nombre;
-        totalPersonas++;
+        //this.cedula = cedula;
+        //this.nombre = nombre;
+        //totalPersonas++;
+        this(cedula, nombre);
     }
 
     public Persona(long cedula) {
-        this.cedula = cedula;
-        this.nombre = "";
-        totalPersonas++;
+        //this.cedula = cedula;
+        //this.nombre = "";
+        //totalPersonas++;
+    	this(cedula,"");
     }
 
     public Persona(String nombre) {
-        this.nombre = "";
-        totalPersonas++;
+        //this.nombre = "";
+        //totalPersonas++;
+    	this(1/1,nombre);
     }
     
     public long getCedula() {
@@ -45,4 +48,8 @@ public class Persona {
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
+    
+    public Persona(){
+    	this(0,"");
+	}
 }
